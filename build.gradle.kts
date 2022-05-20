fec61c42b64d49c79fc79d23a59cef0d14780c2a
Ravindra Jaju <jaju@msync.org>
2022-05-21 00:17:08 +0530

    Updates to deps/tools - clojure, spring, JVM-17, gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "2.5.4"
+    id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -33,9 +33,9 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("io.projectreactor.tools:blockhound:1.0.6.RELEASE")
     implementation("io.projectreactor:reactor-tools")
-    implementation("org.clojure:clojure:1.10.3")
+    implementation("org.clojure:clojure:1.11.1")
     implementation("org.msync:spring-boost:0.2.0-SNAPSHOT")
-    implementation("compojure:compojure:1.6.2")
+    implementation("compojure:compojure:1.6.3")
 }
 
 tasks.register<Copy>("copyClojure") {
